5815392a8b0e247aaca1a9ba9aec851f7819c87b
Graham Phillips <grimephillips@gmail.com>
2017-03-31 22:01:22 +0100

    wip

---
+++ api/routes.js
@@ -0,0 +1,13 @@
+/**
+  manage api requests
+**/
+import express from 'express';
+
+
+const router = express.Router();
+
+router.get('/products', (req, res) => {
+  res.send({data: [] });
+});
+
+export default router;
